d11bb54fe11cfd4c47345ba74ef4741239a94b93
davidcopperfield1991 <david.moh1991@gmail.com>
2022-05-19 01:09:13 +0430

    add second db and today command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -17,6 +18,12 @@ type Star struct {
 	Status   bool
 }
 
+type Dailystar struct {
+	gorm.Model
+	Taskname string
+	Stars    int
+}
+
 func dbde() gorm.DB {
 	dsn := "host=127.0.0.1 user=postgres password=admin dbname=star port=5432 sslmode=disable"
 	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -24,8 +31,15 @@ func dbde() gorm.DB {
 	return *db
 }
 
+func dbdaily() gorm.DB {
+	dsn := "host=127.0.0.1 user=postgres password=admin dbname=star port=5432 sslmode=disable"
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&Dailystar{})
+	return *db
+}
+
 func main() {
-	rootCmd.AddCommand(starCmd, addCmd, listCmd, doneCmd, deleteCmd, helpCmd)
+	rootCmd.AddCommand(starCmd, addCmd, listCmd, doneCmd, deleteCmd, helpCmd, todayCmd)
 	err := rootCmd.Execute()
 	if err != nil {
 		panic(err)
@@ -36,17 +50,24 @@ var rootCmd = &cobra.Command{
 	Use: "",
 }
 
+func updateStar(taskname string, star int) {
+	db := dbde()
+	var records []Star
+	db.Where("taskname = ?", taskname).First(&records)
+	oldstar := records[0].Stars
+	give_ster := star + oldstar
+	db.Model(&Star{}).Where("taskname", taskname).Update("Stars", give_ster)
+
+}
+
 var starCmd = &cobra.Command{
 	Use: "star",
 	Run: func(cmd *cobra.Command, args []string) {
 		title := args[0]
-		var records []Star
-		db := dbde()
-		db.Where("taskname = ?", title).First(&records)
-		oldstar := records[0].Stars
-		newstar, _ := strconv.Atoi(args[1])
-		give_ster := newstar + oldstar
-		db.Model(&Star{}).Where("taskname", title).Update("Stars", give_ster)
+		star, _ := strconv.Atoi(args[1])
+		db := dbdaily()
+		db.Create((&Dailystar{Taskname: title, Stars: star}))
+		updateStar(title, star)
 	},
 }
 
@@ -95,9 +116,29 @@ var listCmd = &cobra.Command{
 	},
 }
 
+var todayCmd = &cobra.Command{
+	Use: "today",
+	Run: func(cmd *cobra.Command, args []string) {
+		db := dbdaily()
+		dt := time.Now()
+		runes := []rune(dt.String())
+		time := string(runes[0:10])
+		az := time + " 00:00:00"
+		ta := time + " 23:59:59"
+		var motaghayer []Star
+		db.Where("created_at BETWEEN ? AND ?", az, ta).Find(&motaghayer)
+		total := 0
+		for i := range motaghayer {
+			fmt.Printf("task: %v -------- status : %v ------- stars: %v \n", motaghayer[i].Taskname, motaghayer[i].Status, motaghayer[i].Stars)
+			total += motaghayer[i].Stars
+		}
+		fmt.Printf("today star : %v \n", total)
+	},
+}
+
 var helpCmd = &cobra.Command{
 	Use: "help",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("star : this command with task name and nimber of star you want to give add one star to task \nadd : this command with task name add task to DB  \ndone : this command with task name change statuse of task to true \nlist: this command show DB rows \ndelete: this command with task name delete task from DB")
+		fmt.Println("star : this command with task name and number of star you want to give add one star to task \nadd : this command with task name add task to DB  \ndone : this command with task name change statuse of task to true \nlist: this command show DB rows \ndelete: this command with task name delete task from DB")
 	},
 }
